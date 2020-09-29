ccce527730ae9b35592824a416847d26b416c4b0
UniQMG <>
2020-09-29 01:52:36 -0700

    custom build system to use runtime-only build of vue

--- pack.sh
+++ pack.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
-# Ignore any electron-or-metadata-specific content
-find -type f |\
-  grep -Ev 'node_modules|\.zip|\.git|electron|package(-lock)?\.json|desktop-manifest\.js|pack.sh' |\
-  zip -r tetrioplus.zip.xpi -9 -u -@
+# Ignores any electron-or-metadata-specific content and builds files for AMO
+# release. YOU DO NOT NEED TO BUILD THE EXTENSION WHILE DEVELOPING!
+
+rm -r ./build
+mkdir ./build
+
+process () {
+  mkdir -p $(dirname "./build/$1")
+  if [[ ! $1 == ./source/lib/* ]] && [ ${1: -3} == ".js" ]; then
+    echo "Building $1"
+    cat $1 | node build.js > ./build/$1
+  else
+    echo "Copying $1"
+    cp $1 ./build/$1
+  fi
+}
+
+files=$(
+  find -type f |\
+  grep -Ev 'build|buildscripttest|node_modules|\.zip|\.xpi|\.git|electron|package(-lock)?\.json|desktop-manifest\.js|pack.sh'
+)
+for file in $files; do
+  process $file
+done
+wait
+
+# Replace vue with the runtime build now that everything is built
+rm ./build/source/lib/vue.js
+mv ./build/source/lib/vue.runtime.js ./build/source/lib/vue.js
+
+cd build
+zip -r ../tetrioplus.xpi -9 -u ./*
