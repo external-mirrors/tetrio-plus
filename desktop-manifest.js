e9cf9076d1ee9186eea8983f40208fabfacfa4c6
UniQMG <2382711-UniQMG@users.noreply.gitlab.com>
2020-12-19 00:18:29 +0000

    Bump patch for desktop v6 and update app.asar hash

--- desktop-manifest.js
+++ desktop-manifest.js
@@ -8,7 +8,7 @@ const manifest = JSON.parse(fs.readFileSync(
 // "firefox settings that firefox will complain about if you add your own entry"
 manifest.browser_specific_settings.desktop_client = {
   // Hash of the uninstall app.asar
-  "vanilla_hash": "36B25280079EBA7C9F83BC67BC0B53B86F57A44A",
+  "vanilla_hash": "FBED5C87693516412FF3CF072EEC7C5A1318D0CC",
   "scripts": [
     "source/shared/migrate.js",
     "source/shared/tpse-sanitizer.js",
