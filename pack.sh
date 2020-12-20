ba57e6d3b5a55af531cb4a1157dea19d913359cc
UniQMG <>
2020-12-20 14:58:49 -0800

    update build script and bump to 0.16.11

--- pack.sh
+++ pack.sh
@@ -18,7 +18,7 @@ process () {
 
 files=$(
   find -type f |\
-  grep -Ev 'build|buildscripttest|node_modules|\.zip|\.xpi|\.git|electron|package(-lock)?\.json|desktop-manifest\.js|pack.sh'
+  grep -Ev 'build|buildscripttest|node_modules|\.zip|\.xpi|\.git|electron|package(-lock)?\.json|desktop-manifest\.js|pack.sh|importer|makeTPSE'
 )
 for file in $files; do
   process $file
