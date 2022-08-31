42d19f5a1f9d24fc52d4a2b24ca8e3f0bc7e2115
UniQMG <>
2022-08-31 15:27:10 -0700

    Add an "import anything" section to data management to expose tpsecore functionality

--- source/panels/settingsImportExport/app.js
+++ source/panels/settingsImportExport/app.js
@@ -1,5 +1,9 @@
 import '../../shared/drop-handler.js';
 import /* non es6 */ '../../shared/migrate.js';
+import init, * as tpsecore from '../../lib/tpsecore.js';
+import importer from '../../importers/import.js';
+import readfiles from '../../shared/filehelper.js';
+await init();
 
 let match = /install=([^=]+)/.exec(new URL(window.location).search);
 if (match) {
@@ -132,6 +136,83 @@ document.getElementById('clearData').addEventListener('click', async () => {
   }
 });
 
+document.getElementById('import-anything').addEventListener('change', async (evt) => {
+  let useExperimental = document.getElementById('use-experimental').checked;
+  let status = document.getElementById('import-anything-status');
+  status.innerText = "Working...";
+  await new Promise(res => setTimeout(res, 50));
+
+  let logs = [];
+  try {
+    let importer = useExperimental ? tpsecoreImport : classicImport;
+    await importer(evt.target, logs);
+    alert(`Import successful!\n\nImport logs:\n${logs.join('\n')}`);
+    status.innerText = "Import successful";
+  } catch(ex) {
+    alert(`Import failed: ${ex}!\n\nImport logs:\n${logs.join('\n')}`);
+    status.innerText = "Import failed";
+    console.error(ex);
+  }
+});
+
+async function classicImport(input, logs) {
+  await importer.automatic(
+    await readfiles(input),
+    browser.storage.local,
+    { log(...msg) {
+      console.log(msg.join(' '));
+      logs.push(msg.join(' '));
+    } }
+  );
+}
+
+async function tpsecoreImport(input, logs) {
+  let advancedMergeLogic = document.getElementById('advanced-merge-logic').checked;
+
+  let reader = new FileReader();
+  let filename = input.files[0].name;
+  reader.readAsArrayBuffer(input.files[0], "UTF-8");
+  reader.onerror = () => alert("Failed to load content pack");
+  let buffer = await new Promise(res => {
+    reader.onload = evt => res(new Uint8Array(reader.result));
+  });
+  console.log(buffer);
+
+  function logger(lvl, msg) {
+    console.log("import", lvl, msg);
+    if (lvl == 'TRACE' || lvl == 'DEBUG') return;
+    let ctx = "";
+    let submsg = msg;
+    if (msg.startsWith('[')) {
+      let braces = 0;
+      do {
+        if (submsg[0] == '[') braces++;
+        if (submsg[0] == ']') braces--;
+        ctx += submsg[0];
+        submsg = submsg.slice(1);
+      } while (braces > 0 && submsg.length > 0);
+    }
+    logs.push(`[${lvl}] Import: ${submsg}\n  context: ${ctx}`);
+  }
+
+  let tpse = tpsecore.create_tpse();
+  logs.push("Importing " + filename);
+  tpsecore.import_file(tpse, { type: 'automatic' }, filename, buffer, logger);
+
+  if (advancedMergeLogic) {
+    logs.push("Re-importing existing content for advanced merge...");
+    let all = await browser.storage.local.get();
+    let all_enc = new TextEncoder('utf8').encode(JSON.stringify(all));
+    tpsecore.import_file(tpse, { type: 'automatic' }, 'current.tpse', all_enc, logger);
+  }
+
+  logs.push("Applying generated TPSE...");
+  let new_tpse = tpsecore.export_tpse(tpse);
+
+  await browser.storage.local.set(JSON.parse(new_tpse));
+  tpsecore.drop_tpse(tpse);
+}
+
 const sampleRate = 44100;
 const channels = 2;
 const quality = 1.0;
@@ -192,4 +273,11 @@ document.getElementById('decompileSfx').addEventListener('click', async () => {
   a.click();
   document.body.removeChild(a);
   status.remove();
+});
+
+let oaiw = document.getElementById('open-auto-import-wiki');
+oaiw.href = 'https://gitlab.com/UniQMG/tetrio-plus/-/wikis/automatic-imports';
+oaiw.addEventListener('click', evt => {
+  evt.preventDefault();
+  window.open('https://gitlab.com/UniQMG/tetrio-plus/-/wikis/automatic-imports');
 });
