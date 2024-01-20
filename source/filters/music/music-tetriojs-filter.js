48667756ebb91a4d2061937056e6dea9b5ea801f
UniQMG <>
2024-01-20 15:39:30 -0800

    fix custom music for tetr.io 6.4.1

--- source/filters/music/music-tetriojs-filter.js
+++ source/filters/music/music-tetriojs-filter.js
@@ -14,12 +14,12 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
 
     // tetrio naively populates the music tweaker using innerHTML
     // so strip all special characters to make it safe
-    for (let song of songs)
-      for (let key of ['name', 'jpname', 'artist', 'jpartist'])
-        song.metadata[key] = song.metadata[key].replace(
-        /[^一-龠ぁ-ゔァ-ヴーa-zA-Z0-9ａ-ｚＡ-Ｚ０-９々〆〤ヶ ]/gu,
-        ''
-      );
+    for (let song of songs) {
+      for (let key of ['name', 'jpname', 'artist', 'jpartist']) {
+        let strip = /[^一-龠ぁ-ゔァ-ヴーa-zA-Z0-9ａ-ｚＡ-Ｚ０-９々〆〤ヶ ]/gu;
+        song.metadata[key] = song.metadata[key].replace(strip, '');
+      }
+    }
 
     let newSongObject = {};
     for (let song of songs) {
@@ -29,7 +29,7 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
         The song song ID is intercepted in the next webRequest handler and
         mapped to the correct custom song content
 
-        Tetr.io concatenates the song name as `/res/bgm/${songname}.mp3`, so
+        TETR.IO concatenates the song name as `/res/bgm/${songname}.mp3`, so
         we add an extra query parameter to "comment out" the extra .mp3
       */
       let key = `akai-tsuchi-wo-funde.mp3?song=${song.id}&tetrioplusinjectioncomment=`;
@@ -40,16 +40,38 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
       }
     }
 
-    let replaced = false;
+    // As of TETR.IO 6.4.1, some keys were renamed. Rather than doing a migration, it's a lot easier to
+    // just rewrite them to match here. Loop positions were also changed from milliseconds to seconds,
+    // and instead of using a length now use an end.
+    for (let song of songs) {
+      song.metadata.untranslatedName = song.metadata.jpname;
+      song.metadata.untranslatedArtist = song.metadata.jpartist;
+      song.metadata.loopStart /= 1000;
+      song.metadata.loopEnd = song.metadata.loopStart + song.metadata.loopLength / 1000;
+      delete song.metadata.jpname;
+      delete song.metadata.jpartist;
+      delete song.metadata.loopLength;
+    }
 
+
+    // Some values used in the music definition were refactored out into constants.
+    // This may not be particularly future-proof, but should work for now.
+    // Also fill in true/false since those will get caught up by the constant-substitution regex.
+    let constants = { true: true, false: false };
+    let regex = /(\w+)=['"](INTERFACE|SPECIAL|BATTLE|CALM|HURT\s*RECORD)['"]/g;
+    for (let [_, constant, value] of src.matchAll(regex))
+      constants[constant] = `"${value}"`;
+    console.log("Extracted music definition constants", constants);
+
+    let replaced = false;
     /**
      * This replacer function locates the songs defined in the source code
      * and the music pools which come immediately after. It attempts to parse
      * the songs (They're not quite json) and add in custom songs.
      */
     src = src.replace(
-      /(const \w+=)({"kuchu.+?(?:(?:{.+?},?)+(?:["A-Za-z\-:]+)?)+?})(,\w+=)({.+?})/,
-      (fullmatch, musicVar, musicJson, musicpoolVar, musicpoolJson) => {
+      /(\w+)=({"kuchu-toshi":{[^}]+}(?:,"?[^"]+"?:{[^}]+})+})(.{0,10000}?)(\w+)=({calm:\[[^\]]+\](?:,battle:\[[^\]]+\])+})/,
+      (fullmatch, musicVar, musicJson, inbetweenBit, musicpoolVar, musicpoolJson) => {
         // Attempt to sanitize the json into actual json
         let sanitizedMusicJson = musicJson
           // What is with these true/false constants?
@@ -59,26 +81,44 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
           .replace(
             /(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9_]+)(['"])?:/g,
             '$1"$3":'
-          );
+          )
+          // Fill in constants
+          .replace(
+            /("[^"]+":)([A-Za-z]+)/g,
+            (_, key, constant) => {
+              if (!constants) console.error("Unknown constant", constant)
+              return key + constants[constant];
+            }
+          )
+          // Add leading 0 to numbers, since json doesn't allow numbers to start with a dot
+          .replace(/("[^"]+":)(\.\d+)/, (_, key, number) => key + '0' + number);
 
         let music;
         if (disableVanillaMusic) {
           music = {};
         } else {
           try {
+            console.log("parsing", { musicJson, sanitizedMusicJson });
             music = JSON.parse(sanitizedMusicJson);
+            for (let song of Object.values(music)) {
+              song.genre = constants[song.genre] || song.genre;
+              song.source = constants[song.source] || song.source;
+            }
+            console.log("parsed", music);
           } catch(ex) {
             console.error(
               'Failed to parse sanitized music pool json',
               sanitizedMusicJson, ex
             );
-            return musicVar + musicJson + musicpoolVar + musicpoolJson;
+            return fullmatch;
           }
         }
 
         Object.assign(music, newSongObject);
         let newMusicJson = JSON.stringify(music);
 
+        // Note: as of 6.4.1, TETR.IO generates 'random' from 'calm' and 'battle' pools automatically.
+        // The 'random' field will be overwritten, and then the whole definition is frozen.
         let newMusicPool = { random: [], calm: [], battle: [] };
         for (let songkey of Object.keys(music)) {
           let song = music[songkey];
@@ -86,15 +126,14 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
             case 'INTERFACE':
             case 'DISABLED':
             case 'OVERRIDE':
+            case 'SPECIAL': // new in TETR.IO 6.4.1, not sure what it does yet.
               break;
 
             case 'CALM':
-              newMusicPool.random.push(songkey);
               newMusicPool.calm.push(songkey);
               break;
 
             case 'BATTLE':
-              newMusicPool.random.push(songkey);
               newMusicPool.battle.push(songkey);
               break;
 
@@ -105,15 +144,15 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
         }
         let newMusicPoolJson = JSON.stringify(newMusicPool);
 
+        fullmatch, musicVar, musicJson, inbetweenBit, musicpoolVar, musicpoolJson
         let rewrite = (
-          musicVar +
-          `new Proxy(${b64Recode(JSON.parse(newMusicJson))}, {
+          musicVar + '=' + `new Proxy(${b64Recode(JSON.parse(newMusicJson))}, {
             get(obj, prop) {
               return (obj[prop] || {
                 name: "Missing song",
-                jpname: "Missing song",
+                untranslatedName: "Missing song",
                 artist: "",
-                jpartist: "",
+                untranslatedArtist: "",
                 genre: 'INTERFACE',
                 source: 'TETR.IO PLUS',
                 loop: false,
@@ -122,8 +161,8 @@ createRewriteFilter("Tetrio.js Music", "https://tetr.io/js/tetrio.js*", {
               })
             }
           })` +
-          musicpoolVar +
-          b64Recode(JSON.parse(newMusicPoolJson))
+          inbetweenBit +
+          musicpoolVar + '=' + b64Recode(JSON.parse(newMusicPoolJson))
         );
         // console.log(
         //   "Rewriting music definition",
