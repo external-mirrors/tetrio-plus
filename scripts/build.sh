6422e3f566c82ab62ab6862864b4134b9266b3fe
UniQMG <>
2026-04-01 06:28:03 -0700

    update rust and add tpsecore.wasm as a direct build artifact

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,7 +19,7 @@ git pull --recurse-submodules # ensure tpsecore is pulled in
 # install rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
-rustup default 1.87.0
+rustup default 1.94.0
 rustup target add wasm32-unknown-unknown
 
 # build it
